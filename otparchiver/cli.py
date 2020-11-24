d3430b3c6381b983784fc59eab5dd53b76bef235
Kevin Wilson <khwilson@gmail.com>
2020-11-23 21:43:24 -0500

    Linting
    Just making some changes for style purposes.

--- otparchiver/cli.py
+++ otparchiver/cli.py
@@ -16,7 +16,6 @@ BASE_URL = "./directory.aspx"
 @click.group()
 def cli():
     """ Various utilities for interacting with the SAMHSA OTP Registry """
-    pass
 
 
 def get_form_data_base(
@@ -49,83 +48,82 @@ def pull_otps(output_filename: Optional[str] = None):
 
     session = requests.session()
     click.echo("Pulling front page...")
-    r = session.get(URL)
-    soup = bs4.BeautifulSoup(r.content, features="html.parser")
+    response = session.get(URL)
+    soup = bs4.BeautifulSoup(response.content, features="html.parser")
 
     form, data = get_form_data_base(soup)
     select = form.find("select")
     data[select["name"]] = "0"
 
     click.echo("Pulling 'all' page...")
-    r = session.post(URL, data=data)
-    soup = bs4.BeautifulSoup(r.content, features="html.parser")
+    response = session.post(URL, data=data)
+    soup = bs4.BeautifulSoup(response.content, features="html.parser")
     form, data = get_form_data_base(soup)
 
     # get the total number of pages to iterate over by clicking the "last page"
     # link and getting the text from the final page link above the table
-    data["__EVENTTARGET"] = 'ctl00$ContentPlaceHolder1$GridView1'
-    data["__EVENTARGUMENT"] = 'Page$Last'
+    data["__EVENTTARGET"] = "ctl00$ContentPlaceHolder1$GridView1"
+    data["__EVENTARGUMENT"] = "Page$Last"
 
     click.echo("Loading last page of results...")
-    r = session.post(URL, data=data)
-    soup = bs4.BeautifulSoup(r.content, features="html.parser")
+    response = session.post(URL, data=data)
+    soup = bs4.BeautifulSoup(response.content, features="html.parser")
 
-    pageCount = int(soup.find_all("table")[1].find("table").find_all("td")[-1].text)
-    click.echo("There are %s pages of results to scrape..." % pageCount)
+    page_count = int(soup.find_all("table")[1].find("table").find_all("td")[-1].text)
+    click.echo(f"There are {page_count} pages of results to scrape...")
 
+    file_headers = [
+        elt.text.lower().replace(" ", "_")
+        for elt in soup.find("tr", {"class": "HEADERSTYLE"}).find_all("th")[:-1]
+    ]
+    file_headers.extend(["latitude", "longitude"])
+    file_data = []
 
-    fileHeaders = [elt.text.lower().replace(' ', '_') for elt in soup.find("tr", { "class": "HEADERSTYLE" }).find_all("th")[:-1]]
-    fileHeaders.extend(['latitude', 'longitude'])
-    fileData = []
-
-    i = 1
-    while i <= pageCount:
+    for i in range(1, page_count + 1):
         # get page i and scrape the table
         form, data = get_form_data_base(soup)
-        data["__EVENTTARGET"] = 'ctl00$ContentPlaceHolder1$GridView1'
-        data["__EVENTARGUMENT"] = 'Page$' + str(i)
+        data["__EVENTTARGET"] = "ctl00$ContentPlaceHolder1$GridView1"
+        data["__EVENTARGUMENT"] = "Page$" + str(i)
 
-        click.echo("Loading page %s of results..." % i)
-        r = session.post(URL, data=data)
-        soup = bs4.BeautifulSoup(r.content, features="html.parser")
+        click.echo(f"Loading page {i} of results...")
+        response = session.post(URL, data=data)
+        soup = bs4.BeautifulSoup(response.content, features="html.parser")
 
         trs = soup.find_all("table")[1].find_all("tr")[3:-2]
 
-        pageData = []
+        page_data = []
 
         for row in trs:
-            rowData = []
+            row_data = []
             for elt in row.find_all("td")[:-1]:
                 # get the primary row data from the text in each td
-                rowData.append(elt.text.strip('\n'))
+                row_data.append(elt.text.rstrip())
 
             # get the latitude and longitude which are hidden in inputs in the phone column
             inputs = row.find_all("input")
             latitude = inputs[0].get("value")
             longitude = inputs[1].get("value")
 
-            rowData.append(latitude)
-            rowData.append(longitude)
+            row_data.append(latitude)
+            row_data.append(longitude)
 
-            pageData.append(rowData)
+            page_data.append(row_data)
 
-        fileData.extend(pageData)
-        i += 1
+        file_data.extend(page_data)
 
-    click.echo(fileData)
+    click.echo(file_data)
 
     # write to file
     click.echo("Processing data...")
-    soup = bs4.BeautifulSoup(r.content, features="html.parser")
+    soup = bs4.BeautifulSoup(response.content, features="html.parser")
     trs = soup.find("table").find_all("tr")
-    headers = [elt.text for elt in trs[0].find_all("th")]
     data = [[elt.text for elt in row.find_all("td")] for row in trs[1:]]
 
     click.echo(f"Writing data to {output_filename}...")
-    with gzip.open(output_filename, "wt") as f:
-        writer = csv.writer(f)
-        writer.writerow(fileHeaders)
-        writer.writerows(fileData)
+    with gzip.open(output_filename, "wt") as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(file_headers)
+        writer.writerows(file_data)
 
     click.echo("Done")
 
